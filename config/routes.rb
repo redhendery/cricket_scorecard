7a3485490671189577913d7a5f44eff6f6bd2125
Red Hendery <redhendery@gmail.com>
2018-05-30 10:42:48 -0700

    add puma config for heroku SSL, complete user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  get 'users/new'
   root 'static_pages#home'
 
 # Static Page Routes
   get '/about', to: 'static_pages#about'
   get '/help', to: 'static_pages#help'
-  get 'signup', to: 'users#new'
+  get '/signup', to: 'users#new'
+  post '/signup',  to: 'users#create'
+
+  resources :users
+
 end
