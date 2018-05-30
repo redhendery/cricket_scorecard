0e155ba18741389d8562eb377444abb3b7913ffd
Red Hendery <redhendery@gmail.com>
2018-05-30 13:19:47 -0700

    complete basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = 'Welcome to the Marin Cricket Score Card'
       redirect_to @user
     else
